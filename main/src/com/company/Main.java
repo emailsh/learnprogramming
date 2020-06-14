fbc25a4a4d9be9f2b2cc502c2054c119a98b872d
Top <top.tanratanavong@gmail.com>
2020-06-14 00:50:59 -0700

    boolean

--- main/src/com/company/Main.java
+++ main/src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        double d1 = 20.00d;
+        double d2 = 80.00d;
+        double result1 = d1 + d2 * 100.00d;
+        double re = result1 % 40.00d;
+
+        boolean isZero =re== 0?true:false;
+        System.out.println(isZero);
+        if (!isZero){
+            System.out.println("Got some remainder");
+ZZ
+        }
     }
 }
