086fe2e673d3162157f3e00e40e2ab4bb2a9cae6
Top <top.tanratanavong@gmail.com>
2020-06-14 03:03:12 -0700

    Methods updated

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -11,6 +11,12 @@ public class Main {
 
         calculateScore(true,800,5,100);
         System.out.println(cal(false,800,5,100));
+
+        displayHighScorePosition("James",5);
+        System.out.println(calculateHighScorePosition(1500));
+        System.out.println(calculateHighScorePosition(900));
+        System.out.println(calculateHighScorePosition(400));
+        System.out.println(calculateHighScorePosition(50));
     }
 
     public static void calculateScore(boolean gameOver,int score,int levelCompleted,int bonus){
@@ -23,9 +29,29 @@ public class Main {
     public static int cal(boolean gameOver,int score,int levelCompleted,int bonus){
         if(!gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-            return 1;
+            return finalScore;
         }
         return -1;
     }
+
+    public static void displayHighScorePosition(String playername, int position){
+        System.out.println(playername + "managed to get into position " + position + "on the high score table");
+
+    }
+    public static int calculateHighScorePosition(int score){
+        if (score >= 1000){
+            return 1;
+        }
+        else if (score >=500){
+            return 2;
+        }
+        else if(score >100){
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
 }
+
+
