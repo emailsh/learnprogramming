e90770b48dee0995d56f7cb3757c618e55e85295
Top <top.tanratanavong@gmail.com>
2020-06-14 01:51:05 -0700

    kAE

--- KeywordsAndExpressions/src/com/company/Main.java
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -3,7 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Test");
+	// a mile is equal to 1.609344 km
+        double kilometres = (100*1.60944);
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This is an expression");
+
+        }
+
     }
 }
